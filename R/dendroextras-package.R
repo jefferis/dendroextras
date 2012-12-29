b82dc1f5181ca8e998b0b20d2acee90348a6b562
Gregory Jefferis <jefferis@gmail.com>
2012-12-29 17:53:12 +0000

    First version of dendroextras
    * only handles label setting/querying at the moment
    * passes check

---
+++ R/dendroextras-package.R
@@ -0,0 +1,8 @@
+#' Extra functions to cut, label and colour dendrogram clusters
+#' 
+#' @name dendroextras-package
+#' @aliases dendroextras
+#' @seealso \code{\link{dendrogram}, \link{hclust}} in \code{\link{stats}} package.
+#' @docType package
+#' @keywords package, dendrogram
+NULL
